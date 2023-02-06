8dd82c2a70ae888eb26a48853123426f2eafcdae
Leire Lasa <leire.lasa.amo@gmail.com>
2023-02-06 16:36:39 +0100

    create new view SeleccionPelicula

--- Reto/src/cine/bbdd/gestor/GestorPeliculas.java
+++ Reto/src/cine/bbdd/gestor/GestorPeliculas.java
@@ -23,36 +23,27 @@ public class GestorPeliculas {
 		ArrayList<Pelicula> ret = null;
 		String sql = FILM_BY_CINEMA + nomCine + "'" + " GROUP BY P.id ORDER BY PR.fecha";
 
-		// La conexion con BBDD
 		Connection connection = null;
 
-		// Vamos a lanzar una sentencia SQL contra la BBDD
-		// Result set va a contener todo lo que devuelve la BBDD
 		Statement statement = null;
 		ResultSet resultSet = null;
 
 		try {
-			// El Driver que vamos a usar
+
 			Class.forName(DBUtils.DRIVER);
 
-			// Abrimos la conexion con BBDD
 			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
 
-			// Vamos a lanzar la sentencia...
 			statement = connection.createStatement();
 			resultSet = statement.executeQuery(sql);
 
-			// No es posible saber cuantas cosas nos ha devuelto el resultSet.
-			// Hay que ir 1 por 1 y guardandolo todo en su objeto Ejemplo correspondiente
 			while (resultSet.next()) {
 
-				// Si es necesario, inicializamos la lista
 				if (null == ret)
 					ret = new ArrayList<Pelicula>();
 
 				Pelicula pelicula = new Pelicula();
 
-				// Sacamos las columnas del RS
 				int id = resultSet.getInt("id");
 				String titulo = resultSet.getString("titulo");
 				int duracion = resultSet.getInt("duracion");
@@ -68,8 +59,6 @@ public class GestorPeliculas {
 				pelicula.setCoste(coste);
 				pelicula.setCaratula(caratula);
 				
-
-				// Lo guardamos en ret
 				ret.add(pelicula);
 			}
 		} catch (SQLException sqle) {
@@ -77,26 +66,22 @@ public class GestorPeliculas {
 		} catch (Exception e) {
 			System.out.println("Error generico - " + e.getMessage());
 		} finally {
-			// Cerramos al reves de como las abrimos
 			try {
 				if (resultSet != null)
 					resultSet.close();
 			} catch (Exception e) {
-				// No hace falta
 			}
 			;
 			try {
 				if (statement != null)
 					statement.close();
 			} catch (Exception e) {
-				// No hace falta
 			}
 			;
 			try {
 				if (connection != null)
 					connection.close();
 			} catch (Exception e) {
-				// No hace falta
 			}
 			;
 		}
