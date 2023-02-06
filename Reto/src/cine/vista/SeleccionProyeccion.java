e22ff22ab8ff2a9ed72799abbcdd2ad68b2bb0ed
Leire Lasa <leire.lasa.amo@gmail.com>
2023-02-06 21:25:43 +0100

    implement datepicker

--- Reto/src/cine/vista/SeleccionProyeccion.java
+++ Reto/src/cine/vista/SeleccionProyeccion.java
@@ -1,8 +1,12 @@
 package cine.vista;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.swing.JFrame;
@@ -13,6 +17,7 @@ import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class SeleccionProyeccion {
 
@@ -65,6 +70,7 @@ public class SeleccionProyeccion {
 		sprFrame.getContentPane().add(sprBtnAtras);
 
 		crearSelectorFecha(sprFrame);
+		
 	}
 	
 	private void crearSelectorFecha(JFrame frame) {
@@ -73,11 +79,33 @@ public class SeleccionProyeccion {
 		p.put("text.today", "Today");
 		p.put("text.month", "Month");
 		p.put("text.year", "Year");
+		
 		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
 		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateComponentFormatter());
-		datePicker.setBounds(110, 100, 200, 25);
+		datePicker.setBounds(70, 120, 200, 25);
 		model.setSelected(true);
 		datePicker.setVisible(true);
-		frame.add(datePicker);
+		frame.getContentPane().add(datePicker);
+		
+		JLabel sprLblCabecera = new JLabel("Seleccione una fecha:");
+		sprLblCabecera.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		sprLblCabecera.setBounds(66, 43, 446, 56);
+		sprFrame.getContentPane().add(sprLblCabecera);
+
+	}
+	
+	private LocalDate confirmarFecha(JDatePickerImpl datePicker) {
+		LocalDate ret = null;
+		Date fechaSeleccionada = (Date) datePicker.getModel().getValue();
+		
+		ret = convertir(fechaSeleccionada);
+		
+		return ret;
+	}
+	
+	private static LocalDate convertir (Date date) {
+	    return date.toInstant()
+	      .atZone(ZoneId.of("GMT"))
+	      .toLocalDate();
 	}
 }
